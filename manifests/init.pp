0adf7940fdc19610c7d87800f55bb6bee4d874b3
Mark Mickan <mark.mickan@netspot.com.au>
2015-07-17 13:48:38 +0930

    Refactor to handle multiple memcached instances
    Without this change, only a single instance of memcached can be instantiated
    and managed by Puppet on a given server.  This is a problem because it's
    often important to use separate instances in order to safely use memcache to
    store information from multiple sources (e.g. different applications or
    different parts of the same application).
    
    Due to the significance of this refactor, and the fact that I'm only
    testing/using this on Ubuntu, I've stripped out support for other operating
    systems (though it shouldn't be overly difficult to put them back in if
    required - other than the memcached::instance defined type being added, the
    API hasn't changed too dramatically).

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,56 +1,86 @@
+#
 # == Class: memcached
 #
-# Manage memcached
+# Deploy and manage memcached.  This class sets sensible defaults for all
+# parameters.
 #
-# == Parameters
-# [* syslog *]
-# Boolean.
-# If true will pipe output to /bin/logger, sends to syslog.
+# === Parameters
+#
+# [*package_ensure*]
+#   (present|absent|<version>) Determines whether or not the package will be
+#   installed, and what version will be installed.
+#
+# [*service_manage*]
+#   Boolean.  Whether or not Puppet should manage the configured memcached
+#   service(s).
+#
+# [*service_restart*]
+#   Boolean.  Whether or not Puppet should restart memcached instances when
+#   their configuration is changed.
+#
+# [*install_dev*]
+#   Boolean.  Determines whether or not the development package will be
+#   installed.
+#
+# [*default_instance*]
+#   Boolean.  Whether or not to manage a default memcached instance.  The
+#   default instance is usually sufficient when only a single memcached
+#   instance is required.
+#
+# See the memcached::instance defined type for an explanation of the
+# remaining parameters.
+#
+# === Example usage
+#
+# Single instance:
+#
+#  class { 'memcached': }
+#
+#
+# Multiple instances:
+#
+#  class { 'memcached': default_instance => false }
+#  memcached::instance { '11211': }
+#  memcached::instance { '11212': }
+#
+#
+# Mutliple instances with descriptive names:
+#
+#  class { 'memcached': default_instance => false }
+#  memcached::instance { 'sessions':
+#    tcp_port => 11211,
+#    udp_port => 11211,
+#  }
+#  memcached::instance { 'muc':
+#    tcp_port => 11212,
+#    udp_port => 11212,
+#  }
 #
 class memcached (
-  $package_ensure  = 'present',
-  $service_manage  = true,
-  $logfile         = $::memcached::params::logfile,
-  $syslog          = false,
-  $pidfile         = '/var/run/memcached.pid',
-  $manage_firewall = false,
-  $max_memory      = false,
-  $item_size       = false,
-  $lock_memory     = false,
-  $listen_ip       = '0.0.0.0',
-  $tcp_port        = 11211,
-  $udp_port        = 11211,
-  $user            = $::memcached::params::user,
-  $max_connections = '8192',
-  $verbosity       = undef,
-  $unix_socket     = undef,
-  $install_dev     = false,
-  $processorcount  = $::processorcount,
-  $service_restart = true,
-  $auto_removal    = false,
-  $use_sasl        = false,
-  $use_registry    = $::memcached::params::use_registry,
-  $registry_key    = 'HKLM\System\CurrentControlSet\services\memcached\ImagePath',
-  $large_mem_pages = false
+  $package_ensure   = 'present',
+  $service_manage   = true,
+  $service_restart  = true,
+  $install_dev      = false,
+  $default_instance = true,
+
+  # defaults for per-instance settings
+  $max_memory       = undef,
+  $item_size        = undef,
+  $lock_memory      = false,
+  $use_sasl         = false,
+  $user             = $::memcached::params::user,
+  $large_mem_pages  = false,
+  $processorcount   = $::processorcount,
+  $auto_removal     = false,
+  $verbosity        = undef,
+  $listen_ip        = '0.0.0.0',
+  $max_connections  = '8192',
 ) inherits memcached::params {
 
-  # validate type and convert string to boolean if necessary
-  if is_string($manage_firewall) {
-    $manage_firewall_bool = str2bool($manage_firewall)
-  } else {
-    $manage_firewall_bool = $manage_firewall
-  }
-  validate_bool($manage_firewall_bool)
   validate_bool($service_restart)
   validate_bool($service_manage)
-
-  validate_bool($syslog)
-
-  # Logging to syslog and file are mutually exclusive
-  # Fail if both options are defined
-  if $syslog and str2bool($logfile) {
-    fail 'Define either syslog or logfile as logging destinations but not both.'
-  }
+  validate_bool($install_dev)
+  validate_bool($default_instance)
 
   if $package_ensure == 'absent' {
     $service_ensure = 'stopped'
@@ -72,52 +102,28 @@ class memcached (
     }
   }
 
-  if $manage_firewall_bool == true {
-    firewall { "100_tcp_${tcp_port}_for_memcached":
-      port   => $tcp_port,
-      proto  => 'tcp',
-      action => 'accept',
-    }
-
-    firewall { "100_udp_${udp_port}_for_memcached":
-      port   => $udp_port,
-      proto  => 'udp',
-      action => 'accept',
+  if $default_instance {
+    memcached::instance { 'default':
+      tcp_port     => 11211,
+      udp_port     => 11211,
+      logfile      => $::memcached::params::logfile,
+      pidfile      => '/var/run/memcached.pid',
     }
   }
 
-  if $service_restart and $service_manage {
-    $service_notify_real = Service[$memcached::params::service_name]
-  } else {
-    $service_notify_real = undef
+  file { '/etc/default/memcached':
+    ensure => 'file',
+    source => 'puppet:///modules/memcached/memcached_default',
   }
 
-  if ( $memcached::params::config_file ) {
-    file { $memcached::params::config_file:
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => template($memcached::params::config_tmpl),
-      require => Package[$memcached::params::package_name],
-      notify  => $service_notify_real,
-    }
+  # in a multi-instance memcached configuration, this service starts all
+  # memcached instances.  However, we start them individually and don't want
+  # to attempt to start them twice so we disable this service on boot and
+  # don't do anything else with it
+  service { 'memcached':
+    ensure  => undef,
+    enable  => false,
+    require => Package[$::memcached::params::package_name],
   }
 
-  if $service_manage {
-    service { $memcached::params::service_name:
-      ensure     => $service_ensure,
-      enable     => $service_enable,
-      hasrestart => true,
-      hasstatus  => $memcached::params::service_hasstatus,
-    }
-  }
-
-  if $use_registry {
-    registry_value{ $registry_key:
-      ensure => 'present',
-      type   => 'string',
-      data   => template($memcached::params::config_tmpl),
-      notify => $service_notify_real,
-    }
-  }
 }
---
+++ manifests/instance.pp
@@ -0,0 +1,145 @@
+#
+# == Define: memcached::instance
+#
+# Deploy and manage an instance of memcached.
+#
+# === Parameters
+#
+# [*tcp_port*]
+#   The TCP port on which the memcached instance will listen.
+#
+# [*udp_port*]
+#   The UDP port on which the memcached instance will listen.
+#
+# [*unix_socket*]
+#   The unix socket on which the memcached instance will listen.
+#
+# [*logfile*]
+#   Logfile for this memcached instance.
+#
+# [*pidfile*]
+#   Pidfile for this memcached instance, or undef to skip creating a
+#   pidfile.
+#
+# [*max_memory*]
+#   Integer with optional '%' suffix.  Maximum memory (in megabytes or as a
+#   percentage of total memory) this memcached instance should use.
+#   Defaults to 95% of total memory.  Use a '%' suffix if specifying a
+#   percentage.
+#
+# [*item_size*]
+#   Integer.  The default size (in megabytes) of each slab page for this
+#   memcache instance.  The default is undef, meaning memcached will use its
+#   default size.
+#
+# [*lock_memory*]
+#   Boolean.  When true, this locks down all paged memory.  Note that this
+#   is a somewhat dangerous option with large caches - consult the memcached
+#   homepage for configuration suggestions.
+#
+# [*user*]
+#   The user to run this memcached instance as.  Note that this module does
+#   not create the user.
+#
+# [*large_mem_pages*]
+#   Boolean.  Try to use large memory pages (if available) for this
+#   memcached instance.
+#
+# [*processorcount*]
+#   Integer.  Number of threads to use to process incoming requests.
+#   Defaults to the number of CPUs.
+#
+# [*auto_removal*]
+#   Boolean.  Set to true to _disable_ automatic removal of items from the
+#   cache when out of memory.  Note that with this set to true, additions to
+#   a full cache will not be possible until adequate space is freed up.
+#
+# [*verbosity*]
+#   Set to undef to make this memcached instance quiet.  Set to "v" to print
+#   out errors and warnings during the event loop, or "vv" to also print
+#   client commands and responses.
+#
+# [*listen_ip*]
+#   IP address on which this memcached instance will listen.
+#
+# [*max_connections*]
+#   Integer.  Allow this many simultaneous connections for this memcached
+#   instance.
+#
+# === Example usage
+#
+#  memcached::instance { '11211': }
+#
+define memcached::instance(
+  $tcp_port        = $name,
+  $udp_port        = $name,
+  $unix_socket     = undef,
+  $logfile         = "/var/log/memcached_${name}.log",
+  $pidfile         = "/var/run/memcached_${name}.pid",
+  $max_memory      = $::memcached::max_memory,
+  $item_size       = $::memcached::item_size,
+  $lock_memory     = $::memcached::lock_memory,
+  $use_sasl        = $::memcached::use_sasl,
+  $user            = $::memcached::user,
+  $large_mem_pages = $::memcached::large_mem_pages,
+  $processorcount  = $::memcached::processorcount,
+  $auto_removal    = $::memcached::auto_removal,
+  $verbosity       = $::memcached::verbosity,
+  $listen_ip       = $::memcached::listen_ip,
+  $max_connections = $::memcached::max_connections,
+) {
+
+  if $max_memory and !is_integer($max_memory) {
+    validate_re($max_memory, '^\d+%$')
+  }
+  if $item_size  { validate_integer($item_size) }
+  validate_integer($processorcount)
+  validate_integer($max_connections)
+  validate_bool($lock_memory)
+  validate_bool($use_sasl)
+  validate_bool($large_mem_pages)
+  validate_bool($auto_removal)
+  validate_re($verbosity, ['^v{0,2}$'])
+  validate_absolute_path($logfile)
+  if $pidfile { validate_absolute_path($pidfile) }
+  if $listen_ip != '' and ! is_ip_address($listen_ip) {
+    fail('listen_ip must be a valid IP address')
+  }
+
+  $_service_name = "memcached_${name}"
+  $_config_file  = "/etc/memcached_${name}.conf"
+
+  if $::memcached::service_restart and $::memcached::service_manage {
+    $_service_notify = Service[$_service_name]
+  } else {
+    $_service_notify = undef
+  }
+
+  file { $_config_file:
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template($::memcached::params::config_tmpl),
+    notify  => $_service_notify,
+  }
+
+  if $::memcached::service_manage {
+    file { "/etc/init.d/${_service_name}":
+      ensure  => 'file',
+      content => template('memcached/memcached_init.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+    } ->
+
+    service { $_service_name:
+      ensure  => $::memcached::service_ensure,
+      enable  => $::memcached::service_enable,
+      require => [
+        Package[$::memcached::params::package_name],
+        File[$_config_file],
+      ],
+    }
+  }
+
+}
